1640aafc7f745d0c0155909977ec46b88efafa00
SPRDesomorphine <PRDesomorphine@users.noreply.github.com>
2019-11-30 17:04:16 +0700

    added bet

--- BlackJack/BlackJack/Form2.Designer.cs
+++ BlackJack/BlackJack/Form2.Designer.cs
@@ -31,22 +31,32 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(432, 37);
+            this.textBox1.Location = new System.Drawing.Point(413, 57);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(88, 26);
+            this.textBox1.Size = new System.Drawing.Size(104, 26);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(427, 9);
+            this.label1.Location = new System.Drawing.Point(411, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 25);
             this.label1.TabIndex = 1;
@@ -56,17 +66,113 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(363, 40);
+            this.label2.Location = new System.Drawing.Point(409, 34);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "баланс:";
             // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(157, 256);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(214, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(173, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Рука Противника";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(203, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ваша Рука";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(157, 313);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(214, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(157, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "ВЗЯТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(157, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "ОСТАВИТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(413, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(104, 26);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(412, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ваша ставка:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "поставить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 416);
+            this.ClientSize = new System.Drawing.Size(532, 445);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -83,5 +189,14 @@
         public System.Windows.Forms.TextBox textBox1;
         public System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
     }
 }
--- BlackJack/BlackJack/Form2.cs
+++ BlackJack/BlackJack/Form2.cs
@@ -15,11 +15,161 @@ namespace BlackJack
         public Form2()
         {
             InitializeComponent();
+            PlayerLogin = label1.Text;
+            for (int i = 1; i <= 13; i++)
+            {
+                Deck[i] = 4;
+            }
+            PlayerBet = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                EnemyHand[i] = 0;
+                PlayerHand[i] = 0;
+            }
+
+            EnemyPosition = 0;
+            PlayerPosition = 0;
+        }
+
+        public int PlayerBalance;
+        public string PlayerLogin;
+        public int PlayerBet; // current player bet
+
+        public int EnemyPosition; // position to place a new card
+        public int PlayerPosition; // position to place a new card
+
+        public int[] Deck = new int[14]; // number of cards left in the deck, 0 - nothing, 1 - 2, 2 - 3 ... 10 - Jack, 11 - Queen, 12 - King, 13 - Ace
+        public int[] EnemyHand = new int[9]; // enemy cards in hand
+        public int[] PlayerHand = new int[9]; // player cards in hand
+
+        public bool BetChecker() // checks bet
+        {
+            PlayerBalance = Convert.ToInt32(textBox1.Text);
+            if ((PlayerBet > PlayerBalance) || (PlayerBet < 0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public string NumberConverter(int number) // converts number to visual card
+        {
+            if (number < 10)
+            {
+                return Convert.ToString(number + 1);
+            }
+            else
+            {
+                switch (number)
+                {
+                    case 10:
+                        return "Jack";
+                        break;
+                    case 11:
+                        return "Queen";
+                        break;
+                    case 12:
+                        return "King";
+                        break;
+                    case 13:
+                        return "Ace";
+                        break;
+                    default:
+                        return "Error";
+                        break;
+                }
+            }
+        }
+
+        public int RandomNumberGenerator(Random Random)
+        {
+            return Random.Next(1, 13);
+        }
+
+        public void HandEraser() // makes no cards in hand
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                EnemyHand[i] = 0;
+                PlayerHand[i] = 0;
+            }
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            for (int i = 1; i < 14; i++)
+            {
+                Deck[i] = 4;
+            }
+        }
+
+        public void StarterHandGeneration() // generates starter hand
+        {
+            Random Random = new Random();
+
+            EnemyPosition = 2;
+            PlayerPosition = 2;
+
+            int temporaryRandomNumber = RandomNumberGenerator(Random);
+
+            for (int i = 0; i < 2; i++) // enemy hand generator
+            {
+                while (Deck[temporaryRandomNumber] == 0)
+                {
+                    temporaryRandomNumber = RandomNumberGenerator(Random);
+                }
+
+                Deck[temporaryRandomNumber]--;
+                textBox2.Text = textBox2.Text + ' ' + NumberConverter(temporaryRandomNumber);
+
+                temporaryRandomNumber = RandomNumberGenerator(Random);
+            }
+
+            temporaryRandomNumber = RandomNumberGenerator(Random);
+
+            for (int i = 0; i < 2; i++) // player hand generator
+            {
+                while (Deck[temporaryRandomNumber] == 0)
+                {
+                    temporaryRandomNumber = RandomNumberGenerator(Random);
+                }
+
+                Deck[temporaryRandomNumber]--;
+                textBox3.Text = textBox3.Text + ' ' + NumberConverter(temporaryRandomNumber);
+
+                temporaryRandomNumber = RandomNumberGenerator(Random);
+            }
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e) // do a bet
+        {
+            PlayerBet = Convert.ToInt32(textBox4.Text);
+
+            if (BetChecker())
+            {
+                textBox4.ReadOnly = true;
+                PlayerBalance -= PlayerBet;
+                textBox1.Text = Convert.ToString(PlayerBalance);
+                button3.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Ставка неверна!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            HandEraser();
+            StarterHandGeneration();
         }
     }
 }
